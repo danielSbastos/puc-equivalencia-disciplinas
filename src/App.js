be291d952e16710237e5fd9140588115e5063ed5
danielSbastos <daniel.bastos1710@gmail.com>
2022-06-09 09:56:51 -0300

    Invert 'a cursar' and 'extras' columns

--- src/App.js
+++ src/App.js
@@ -21,58 +21,60 @@ const date = () => {
 
 const Pdf = React.forwardRef(({ subjects, equivalences }, ref) => {
   return (
-    <div className='pdf' ref={ref}>
-      <div className="text-center">
-        <img src="image1.jpg" width={'45%'} alt="puc" />
-        <p style={{ marginTop: '1%' }}><b>Centro de Registros Acadêmicos</b></p>
+    <>
+      <div className='pdf' ref={ref}>
+        <div className="text-center">
+          <img src="image1.jpg" width={'45%'} alt="puc" />
+          <p style={{ marginTop: '1%' }}><b>Centro de Registros Acadêmicos</b></p>
+        </div>
+
+        <label>Prezado Coordenador,</label>
+        <p>Favor verificar se a equivalência solicitada pode ser efetivada.</p>
+
+        Assinale um dos quadros abaixo:<br />
+        <input type="checkbox" /> Não, a equivalência não pode ser considerada.<br />
+        <input type="checkbox" /> Sim. A equivalência deve prevalecer para todos os casos e deve ser lançada na tabela de equivalência.
+
+        <Table bordered style={{ marginTop: '0.5%', fontSize: '14px' }}>
+          <thead>
+            <tr>
+              <th>Disciplinas do curso</th>
+              <th>CH</th>
+              <th>Código</th>
+              <th>Disciplinas equivalentes</th>
+              <th>CH</th>
+              <th>Código</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.entries(equivalences).map((equivalence) => {
+              const extra = subjects.subjectsExtra[subjects.extraIndex[equivalence[1]]]
+              const missing = subjects.subjectsMissing[subjects.missingIndex[equivalence[0]]]
+              return (
+                <tr key={equivalence[0]}>
+                  <td>{missing.name}</td>
+                  <td>{missing.hours}</td>
+                  <td>{missing.id}</td>
+                  <td>{extra.name}</td>
+                  <td>{extra.hours}</td>
+                  <td>{extra.id}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </Table>
+
+        <input type="checkbox" checked /> Sim. A equivalência, entretanto, só pode ser considerada nestes casos específicos.<br />
+        <label style={{ marginTop: '1%', marginRight: '4%' }}>Aluno(a): Daniel Schlickmann Bastos</label>
+        <label>Matrícula: 696777</label><br />
+        <label style={{ marginBottom: '4%' }}>Data: {date()}</label>
+        <br />
+
+        _________________________________________
+        <p>Assinatura e carimbo da Coordenação do Curso</p>
+
       </div>
-
-      <label>Prezado Coordenador,</label>
-      <p>Favor verificar se a equivalência solicitada pode ser efetivada.</p>
-
-      Assinale um dos quadros abaixo:<br />
-      <input type="checkbox" /> Não, a equivalência não pode ser considerada.<br />
-      <input type="checkbox" /> Sim. A equivalência deve prevalecer para todos os casos e deve ser lançada na tabela de equivalência.
-
-      <Table bordered style={{ marginTop: '0.5%', fontSize: '14px' }}>
-        <thead>
-          <tr>
-            <th>Disciplinas do curso</th>
-            <th>CH</th>
-            <th>Código</th>
-            <th>Disciplinas equivalentes</th>
-            <th>CH</th>
-            <th>Código</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.entries(equivalences).map((equivalence) => {
-            const extra = subjects.subjectsExtra[subjects.extraIndex[equivalence[0]]]
-            const missing = subjects.subjectsMissing[subjects.missingIndex[equivalence[1]]]
-            return (
-              <tr key={equivalence[0]}>
-                <td>{missing.name}</td>
-                <td>{missing.hours}</td>
-                <td>{missing.id}</td>
-                <td>{extra.name}</td>
-                <td>{extra.hours}</td>
-                <td>{extra.id}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
-
-      <input type="checkbox" checked /> Sim. A equivalência, entretanto, só pode ser considerada nestes casos específicos.<br />
-      <label style={{ marginTop: '1%', marginRight: '4%' }}>Aluno(a): Daniel Schlickmann Bastos</label>
-      <label>Matrícula: 696777</label><br />
-      <label style={{ marginBottom: '4%' }}>Data: {date()}</label>
-      <br />
-
-      _________________________________________
-      <p>Assinatura e carimbo da Coordenação do Curso</p>
-
-    </div>
+    </>
   );
 });
 
@@ -92,7 +94,7 @@ function App() {
 
     if (isExtra) {
       if (missingSelected) {
-        setEquivalences({ ...equivalences, [id]: missingSelected })
+        setEquivalences({ ...equivalences, [missingSelected]: id })
         setExtraSelected(null)
         setMissingSelected(null)
       } else {
@@ -100,7 +102,7 @@ function App() {
       }
     } else {
       if (extraSelected) {
-        setEquivalences({ ...equivalences, [extraSelected]: id })
+        setEquivalences({ ...equivalences, [id]: extraSelected })
         setExtraSelected(null)
         setMissingSelected(null)
       } else {
@@ -126,34 +128,59 @@ function App() {
         <hr />
         <Row>
           <Col md={7}>
-            <h3>Disciplinas extracurriculares</h3>
+            <h3>Disciplinas a cursar</h3>
             <Table striped bordered hover>
               <thead>
                 <tr>
                   <th>Código</th>
                   <th style={{ width: '33%' }}>Disciplina</th>
-                  <th>Nota</th>
                   <th style={{ width: '10%' }}>CH</th>
                   <th style={{ width: '33%' }} colSpan="2">Equivalência</th>
                 </tr>
               </thead>
               <tbody>
-                {subjects.subjectsExtra.map(extra => {
+                {subjects.subjectsMissing.map(missing => {
                   let equivalence;
 
-                  if (equivalences[extra.id]) {
-                    equivalence = equivalences[extra.id] + " - " + subjects.subjectsMissing[subjects.missingIndex[equivalences[extra.id]]].name;
+                  if (equivalences[missing.id]) {
+                    equivalence = equivalences[missing.id] + " - " + subjects.subjectsExtra[subjects.extraIndex[equivalences[missing.id]]].name;
                   }
 
-                  const clicked =  Object.keys(equivalences).includes(extra.id) || (extraSelected === extra.id);
+                  const clicked =  Object.keys(equivalences).includes(missing.id) || (missingSelected === missing.id);
                   return (
-                    <tr className={(clicked || equivalence)? "subject-selected": ""} onClick={(event) => handleEquivalence(extra.id, true, event)} key={extra.id}>
+                    <tr className={(clicked || equivalence)? "subject-selected": ""} onClick={(event) => handleEquivalence(missing.id, false, event)} key={missing.id}>
+                      <td>{missing.id}</td>
+                      <td>{missing.name}</td>
+                      <td>{missing.hours}</td>
+                      <td>{equivalences[missing.id] && equivalence}</td>
+                      <td>{equivalences[missing.id] && <TrashFill onClick={() => handleRemoveEquivalence(missing.id)}/>}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </Table>
+          </Col>
+          <Col md={{ span: 5 }}>
+            <h3>Disciplinas extracurriculares</h3>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Código</th>
+                  <th>Disciplina</th>
+                  <th>Nota</th>
+                  <th>CH</th>
+                </tr>
+              </thead>
+              <tbody>
+                {subjects.subjectsExtra.map(extra => {
+                  const clicked =  Object.values(equivalences).includes(extra.id) || (extraSelected === extra.id);
+
+                  return (
+                    <tr className={clicked ? "subject-selected": ""} onClick={(event) => handleEquivalence(extra.id, true, event)} key={extra.id}>
                       <td>{extra.id}</td>
                       <td>{extra.name}</td>
                       <td>{extra.grade}</td>
                       <td>{extra.hours}</td>
-                      <td>{equivalences[extra.id] && equivalence}</td>
-                      <td>{equivalences[extra.id] && <TrashFill onClick={() => handleRemoveEquivalence(extra.id)}/>}</td>
                     </tr>
                   )
                 })}
@@ -164,31 +191,6 @@ function App() {
               content={() => componentRef.current}
             />
             <Pdf ref={componentRef} equivalences={equivalences} subjects={subjects} />
-          </Col>
-          <Col md={{ span: 5 }}>
-            <h3>Disciplinas a cursar</h3>
-            <Table striped bordered hover>
-              <thead>
-                <tr>
-                  <th>Código</th>
-                  <th>Disciplina</th>
-                  <th>CH</th>
-                </tr>
-              </thead>
-              <tbody>
-                {subjects.subjectsMissing.map(missing => {
-                  const clicked =  Object.values(equivalences).includes(missing.id) || (missingSelected === missing.id);
-
-                  return (
-                    <tr className={clicked ? "subject-selected": ""} onClick={(event) => handleEquivalence(missing.id, false, event)} key={missing.id}>
-                      <td>{missing.id}</td>
-                      <td>{missing.name}</td>
-                      <td>{missing.hours}</td>
-                    </tr>
-                  )
-                })}
-              </tbody>
-            </Table>
           </Col>
         </Row>
       </Container>
